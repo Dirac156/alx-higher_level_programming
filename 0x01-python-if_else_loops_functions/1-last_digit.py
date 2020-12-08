c4a67f124d9465d43941c905175a729633385d64
Dirac Murairi <diracm8@gmail.com>
2020-12-08 17:54:25 +0200

    else to elif

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number >= 0:
     last = number % 10
-else number < 0:
+elif number < 0:
     last = number % -10
 if last == 0:
     print("Last digit of {} is {} and is 0".format(number, last))

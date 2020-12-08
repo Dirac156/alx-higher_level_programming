d0d114a6e7890488f8c145d22d137ef92c31e63a
Dirac Murairi <diracm8@gmail.com>
2020-12-08 17:25:18 +0200

    add increment in task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -6,6 +6,8 @@ def remove_char_at(str, n):
     new = ""
     for element in str:
         if i == n:
+            i += 1
             continue
         new += str[i]
+        i += 1
     return new

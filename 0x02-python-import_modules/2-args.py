1f816c971cb86e9a13a9e1548d26671a2c9f0f5f
Dirac Murairi <diracm8@gmail.com>
2020-12-11 07:14:44 +0200

    fix output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         if (len(sys.argv)) == 2:
-            print("{:d} argument".format((len(sys.argv)) - 1))
+            print("{:d} argument:".format((len(sys.argv)) - 1))
             print("{:d}: {}".format(1, sys.argv[1]))
         else:
             print("{:d} arguments:".format((len(sys.argv)) - 1))

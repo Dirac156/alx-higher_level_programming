cefef985c1107d9bd83e846ab946b9bc294f8ce1
Dirac Murairi <diracm8@gmail.com>
2020-12-15 12:44:15 +0200

    fix the error in task 0. now it shoul loop over my_list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     new_list = my_list[:]
     new_list = new_list.reverse()
     for numb in new_list:
-        print("{}".format(numb))
+        print("{:d}".format(numb))

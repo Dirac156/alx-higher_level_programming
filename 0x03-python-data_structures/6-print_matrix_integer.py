7eee430968978093e6d730b76f8da15488f4c1a1
Dirac Murairi <diracm8@gmail.com>
2020-12-15 17:29:25 +0200

    add a condition before finding the matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+        return
     for lst in matrix:
         for numb in lst:
             l = len(lst) - 1

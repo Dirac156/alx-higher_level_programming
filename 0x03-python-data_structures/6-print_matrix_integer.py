65010cda10b1ec0dbf76c5448bb0b322ded74868
Dirac Murairi <diracm8@gmail.com>
2020-12-15 16:58:32 +0200

    add tuple

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for numb in lst:
             l = len(lst) - 1
             if lst.index(numb) == l:
-                print("{:d}".format(numb), end = "")
+                print("{:d}".format(numb))
             else:
                 print("{:d}".format(numb), end=' ')
-        print()

8009399afb2dd3e4810c13b351c5a8174cee5a1f
Dirac Murairi <diracm8@gmail.com>
2020-12-17 18:33:24 +0200

    use the same dic and update it

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = {}
-    for keys in a_dictionary.keys():
-        if keys == key:
-            new_dic[key] = value
-        else:
-            new_dic[keys] = a_dictionary[keys]
-    return new_dic
+    a_dictionary[key] = value
+    return a_dictionary

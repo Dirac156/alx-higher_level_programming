980aa2ba109bd96c7d860c382a5f569c217b680b
Dirac Murairi <diracm8@gmail.com>
2020-12-17 17:36:31 +0200

    change mul to power operation and remove test made

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,10 +5,6 @@ def square_matrix_simple(matrix=[]):
     else:
         new_matrix = []
         for column in matrix:
-            new_column = list(map(lambda x : x*2, column))
+            new_column = list(map(lambda x : x**2, column))
             new_matrix.append(new_column)
         return new_matrix
-
-print(square_matrix_simple([[2, 4], [5, 6], [10, 20]]))
-print(square_matrix_simple([]))
-print(square_matrix_simple(None))

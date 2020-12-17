38845a22cf7b6b6d95afcf5912a62d816172350d
Dirac Murairi <diracm8@gmail.com>
2020-12-17 18:29:12 +0200

    remove copy and use loop

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = a_dictionary.copy()
-    new_dic[key] = value
+    new_dic = {}
+    for keys, values in a_dictionary.items():
+        if keys == key:
+            new_dic[keys] = value
+        else:
+            new_dic[keys] = values
     return new_dic

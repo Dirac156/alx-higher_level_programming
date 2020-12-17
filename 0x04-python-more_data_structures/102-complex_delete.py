a03b08fb45f0115c2a6520fdfb566529425e4813
Dirac Murairi <diracm8@gmail.com>
2020-12-17 20:48:16 +0200

    change when to del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    lst = []
     for k, v in a_dictionary.items():
         if v == value:
-            del a_dictionary[k]
+            lst.append(k)
+    for k in lst:    
+        del a_dictionary[k]
     return a_dictionary

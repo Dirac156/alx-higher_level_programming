3f561a70d5af601f47e3c6646a69412c59c57b39
Dirac Murairi <diracm8@gmail.com>
2020-12-17 20:23:20 +0200

    style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False or roman_string is None:
+    r_s = roman_string
+    if isinstance(r_s, str) is False or r_s is None or r_s == "":
         return 0
     numb_r = 0
     lst = []
     for l in roman_string:
         lst.append(l)
     i = 0
-    roman_symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+    roman_s = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                'C': 100, 'D': 500, 'M': 1000}
-    for symbol in lst:
-        if symbol not in roman_symbol:
+    for s in lst:
+        if s not in roman_s:
             return 0
         else:
-            idx = lst.index(symbol)
-            if symbol == lst[-1] or \
-                roman_symbol[symbol] >= roman_symbol[lst[idx + 1]]:
-                numb_r += roman_symbol[symbol]
+            idx = lst.index(s)
+            if s == lst[-1] or roman_s[s] >= roman_s[lst[idx + 1]]:
+                numb_r += roman_s[s]
             else:
-                numb_r -= roman_symbol[symbol]
+                numb_r -= roman_s[s]
     return numb_r

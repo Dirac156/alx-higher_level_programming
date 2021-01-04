51784d2fbb4ac1a5d92de2ea866c618306982d50
Dirac Murairi <d.murairi@alustudent.com>
2021-01-04 15:15:13 +0200

    fix stying with pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     for numb in range(0, x):
         try:
-            print("{:d}".format(my_list[numb]), end = "")
+            print("{:d}".format(my_list[numb]), end="")
             printed += 1
-        except:
+        except Exception:
             pass
     print("")
     return printed

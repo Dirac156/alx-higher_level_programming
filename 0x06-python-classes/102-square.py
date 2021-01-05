df45d247a64e0d5e097a98a3b56b2691db066808
Dirac Murairi <d.murairi@alustudent.com>
2021-01-05 20:27:17 +0200

    task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+This file contains the class Square
+"""
+
+
+class Square():
+    """
+    This is the Square class
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        return the area of a square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.__size == other.__size
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, Square):
+            return self.__size != other.__size
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.__size < other.__size
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.__size > other.__size
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, Square):
+            return self.__size <= other.__size
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, Square):
+            return self.__size >= other.__size
+        return False

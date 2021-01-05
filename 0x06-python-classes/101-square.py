7308cad6698b43c8f7743e6018fd3c8acfbde835
Dirac Murairi <d.murairi@alustudent.com>
2021-01-05 20:04:12 +0200

    task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""
+This file contains the class Square
+"""
+
+
+class Square():
+    """
+    This is the Square class
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
+        if isinstance(position, tuple) is not True or len(position) != 2:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        elif type(position[0]) != int or type(position[1]) != int:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        else:
+            self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for numb in range(0, self.__position[1]):
+                print("")
+            for el in range(0, self.__size):
+                for numb in range(0, self.__position[0]):
+                    print(" ", end="")
+                for el_2 in range(0, self.__size):
+                    print("#", end="")
+                print("")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) is not True or len(value) != 2:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        elif isinstance(value[0], int) is not True or type(value[1]) != int:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        else:
+            self.__position = value
+
+    def __str__(self):
+        s = ""
+        if self.__size == 0:
+            s += "\n"
+        else:
+            for numb in range(0, self.__position[1]):
+                s += "\n"
+            for el in range(0, self.__size):
+                for numb in range(0, self.__position[0]):
+                    s += " "
+                for el_2 in range(0, self.__size):
+                    s += "#"
+                if el != self.__size - 1:
+                    s += "\n"
+        return s

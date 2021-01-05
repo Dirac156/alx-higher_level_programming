604f96ed11d36ee47f4fe74a80a75f98814d5dc5
Dirac Murairi <d.murairi@alustudent.com>
2021-01-05 17:09:55 +0200

    add documentation on task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This file contains the class Square
+"""
+
+
 class Square():
+    """
+    This is the class square
+    """
     def __init__(self, size):
         self.__size = size

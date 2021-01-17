e48631fe09f1d38bb7d4ffb38fa93eb0612b6fff
= <=>
2021-01-17 07:13:37 +0200

    fix formating

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""
-This file contains the class Rectangle
-"""
-
-
-class Rectangle():
-    """
-    This is the class rectangle
-    """
+class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,13 +10,12 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
-        else:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -32,10 +23,9 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
-        else:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value

a6e4e0640307a3e1cb7a790a00a9721704c27514
= <=>
2021-01-18 21:54:50 +0200

    dirac

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" Geometry class """
+"""
+geometry
+"""
 
 
-class BaseGeometry():
-    """ This is the skeleton for geo class"""
+class BaseGeometry:
+    """BaseGeometry class"""
+
     def area(self):
+        """method for calculated area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        elif value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

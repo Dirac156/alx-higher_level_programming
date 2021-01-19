f6812006d5ad08c296dc95b406b0753140ee7614
= <=>
2021-01-19 19:39:36 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write in a file"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        myFile.write(text)
+    return len(text)

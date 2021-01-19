5b69665403a3a5fa82f2df9cca2228947a9bf66e
= <=>
2021-01-19 21:05:50 +0200

    try json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """load json"""
     with open(filename, encoding="utf-8") as f:
-        return json.loads(f.read())
+        return json.load(f)

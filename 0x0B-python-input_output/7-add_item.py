010e55a840748b19d0cbe43d55b4891091bba77d
= <=>
2021-01-19 21:10:41 +0200

    fix error white space

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     File = load_from_json_file("add_item.json")
-except FileNotFoundError:
+except Exception:
     File = []
 
 for idx in range(1, len(sys.argv)):
     File.append(sys.argv[idx])
 save_to_json_file(File, "add_item.json")
-        

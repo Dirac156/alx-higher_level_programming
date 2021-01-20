71f95c93438fd0718a0459432a067eb014d27007
= <=>
2021-01-20 19:07:35 +0200

    style

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -14,17 +14,21 @@ class MyInt(int):
 
     @property
     def my_int(self):
+        """something"""
         return self.__my_int
 
     @my_int.setter
     def my_int(self, my_int):
+        """something"""
         if type(my_int) is not int:
             raise TypeError("my_int must be an integer")
         else:
             self.__my_int = my_int
 
     def __eq__(self, other):
+        """eq rebel"""
         return !(self.my_int == other)
 
     def __ne__(self, other):
+        """ne rebel"""
         return !(self.my_int != other)

80574526435ac1e70799f12972466b290ca83587
= <=>
2021-01-26 20:34:35 +0200

    styling

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """construct square method"""
         super().__init__(size, size, x, y, id)
-        self.size = size
-    
+        self.__size = size
+
     @property
     def size(self):
         """ size property """
@@ -22,12 +22,12 @@ class Square(Rectangle):
         """ size setter """
         self.width = value
         self.height = value
-        self.size = value
+        self.__size = value
 
     def __str__(self):
         """overide parent's str method"""
         id = self.id
-        size = self.size
+        size = self.__size
         x = self.x
         y = self.y
         return "[Square] ({}) {}/{} - {}".format(id, x, y, size)

55eab1d4a776f77a2227246f4f6c5e094ff607d1
= <=>
2021-03-22 20:50:20 +0200

    indentation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -14,5 +14,6 @@ const myArray = process.argv.slice(2, process.argv.length);
 if (myArray < 2) {
   console.log(0);
 } else {
-  console.log(getArgument(myArray).sort().reverse()[1]);
+  console.log(getArgument(myArray)
+    .sort().reverse()[1]);
 }

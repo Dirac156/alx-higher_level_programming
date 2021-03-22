f7109c6cdc239394c55999091880593034dd4ea8
= <=>
2021-03-22 20:36:08 +0200

    uses built in function to execute the process

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,30 +2,8 @@
 
 const process = require('process');
 
-function parseStringToNumber (myArray) {
-  for (let i = 0; i < myArray.length; i++) {
-    myArray[i] = Number(myArray[i]);
-  }
-  return myArray;
-}
-
-function findSecondBiggest (myArray) {
-  myArray = parseStringToNumber(myArray);
-  const biggest = Math.max(...myArray);
-  let second = myArray[0];
-  for (let i = 1; i < myArray.length; i++) {
-    if (myArray[i] > second && myArray[i] !== biggest) {
-      second = myArray[i];
-    }
-  }
-
-  return second;
-}
-
-if (isNaN(process.argv[3])) {
+if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const allInput = process.argv.slice(2, process.argv.length);
-  const secondBiggest = findSecondBiggest(allInput);
-  console.log(secondBiggest);
+  console.log(process.argv.sort().reverse()[1]);
 }

8efe2d8f4da778657a241df428d327ca0cb0e5dd
= <=>
2021-03-22 18:33:24 +0200

    when multiple arguments are passed. added condition

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@
 const process = require('process');
 if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length !== 2) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

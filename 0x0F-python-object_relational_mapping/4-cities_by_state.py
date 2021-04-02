fdf13159f4edc6cec4c49e9d4e4977d5c65e2333
= <=>
2021-04-02 21:29:58 +0200

    completed task 10

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(user=argv[1],
+                               passwd=argv[2],
+                               db=argv[3])
+    curs = database.cursor()
+    curs.execute("SELECT cities.id, cities.name, states.name\
+                  FROM cities INNER JOIN states ON states.id=cities.state_id\
+                  ORDER BY cities.id ASC")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    database.close()
